d4fcc3821052e0e8a90d000d19fb5a4ac5fdcfef
diego-omini <diego.omini@gmail.com>
2017-01-11 11:46:19 +0100

    Aggiunti filtri in listaUtenti
    Aggiunto la possibilità di filtrare i risultati della query, manca la
    parte di modificare il controllo dell'id (>, <, >=, <=), ora presente
    solo =

--- 02_Codice/htdocs/FormazioneDocenti/TierData/DataModel/listaUtenti.php
+++ 02_Codice/htdocs/FormazioneDocenti/TierData/DataModel/listaUtenti.php
@@ -3,24 +3,30 @@
   require_once "$__ROOT__/tierData/DbInterface/mysqli.php";
 
   //Esempi di chiamata:
-  //$lista1 = new listaUtenti(null, nomegruppo, idgruppo);
-  //$lista1 = new listaUtenti(nomeutente);
-  //$lista1 = new listaUtenti(null, nomegruppo);
-  //
+  //$lista1 = new listaUtenti($mysqli_conn, null, nomegruppo, idgruppo);
+  //$lista1 = new listaUtenti($mysqli_conn);
+  //$lista1 = new listaUtenti($mysqli_conn, null, nomegruppo);
 
   class listaUtenti {
-    public $filtroNomeUtente = "*";
-    public $filtroNomeGruppo = "*";
-    public $filtroIdGruppo = "*";
+    public $filtroNomeUtente = "";
+    public $filtroNomeGruppo = "";
+    public $filtroIdGruppo = "";
     public $result;
 
-    public function __construct($mysqli_conn, $argument1 = "*", $argument2 = "*", $argument3 = "*") {
+    public function __construct($mysqli_conn, $filtroNomeUtente = "", $filtroNomeGruppo = "", $filtroIdGruppo = "") {
       $sql = "
-      SELECT * FROM utenti
-      WHERE nomeUtente like '%' AND Gruppi_idGruppo like '%';
+      SELECT utenti.*
+      FROM utenti, gruppi
+      WHERE utenti.nomeUtente like'%$filtroNomeUtente%'
+            AND gruppi.descrizione like '%$filtroNomeGruppo%'
+            AND utenti.Gruppi_idGruppo like '%$filtroIdGruppo%'
+            AND utenti.Gruppi_idGruppo = gruppi.idGruppo;
       ";
 
       $this->result = $mysqli_conn->query($sql);
+      $this->filtroNomeUtente = $filtroNomeUtente;
+      $this->filtroNomeGruppo = $filtroNomeGruppo;
+      $this->filtroIdGruppo = $filtroIdGruppo;
     }
 
     public function stampaLista() {
--- 02_Codice/htdocs/FormazioneDocentiTest/testquery.php
+++ 02_Codice/htdocs/FormazioneDocentiTest/testquery.php
@@ -2,10 +2,9 @@
   require $_SERVER['DOCUMENT_ROOT']."/FormazioneDocenti/baseUrl.php";
   require_once "$__ROOT__/tierData/DataModel/listaUtenti.php";
 
-  $lista1 = new listaUtenti($mysqli_conn);
+  $lista1 = new listaUtenti($mysqli_conn, "test1");
+  //$lista1 = new listaUtenti($mysqli_conn, null, "Amministratori");
+  //$lista1 = new listaUtenti($mysqli_conn, "test0", null, 3);
+  //$lista1 = new listaUtenti($mysqli_conn, "test", "Docenti", 1);
   $lista1->stampaLista();
-
-  //$lista1 = new listaUtenti(null, nomegruppo, idgruppo);
-  //$lista1 = new listaUtenti(nomeutente);
-  //$lista1 = new listaUtenti(null, nomegruppo);
 ?>
