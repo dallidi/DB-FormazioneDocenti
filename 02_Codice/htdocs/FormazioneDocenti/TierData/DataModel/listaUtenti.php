f9ac81dac7d4799d270583649d97d8dc10aa3eac
diego-omini <diego.omini@gmail.com>
2017-02-07 15:39:37 +0100

    Corretto query
    Sostituito mysqli con PDO e ora ritorna array invece di stampare

--- 02_Codice/htdocs/FormazioneDocenti/TierData/DataModel/listaUtenti.php
+++ 02_Codice/htdocs/FormazioneDocenti/TierData/DataModel/listaUtenti.php
@@ -1,42 +1,46 @@
 <?php
 
   require_once "$__ROOT__/tierData/DbInterface/mysqli.php";
+  require_once "$__ROOT__/tierData/dbInterface/CommonDB.php";
 
   //Esempi di chiamata:
   //$lista1 = new listaUtenti($mysqli_conn, null, nomegruppo, idgruppo);
   //$lista1 = new listaUtenti($mysqli_conn);
   //$lista1 = new listaUtenti($mysqli_conn, null, nomegruppo);
 
-  class listaUtenti {
+  class ListaUtenti {
     public $filtroNomeUtente = "";
     public $filtroNomeGruppo = "";
     public $filtroIdGruppo = "";
     public $result;
+    public $lista_utenti = array();
 
-    public function __construct($mysqli_conn, $filtroNomeUtente = "", $filtroNomeGruppo = "", $filtroIdGruppo = "") {
+    public function __construct(&$lista_utenti, $mysqli_conn, $filtroNomeUtente = "", $filtroNomeGruppo = "", $filtroIdGruppo = "", $operazione = "=") {
+      global $db;
       $sql = "
       SELECT utenti.*
       FROM utenti, gruppi
       WHERE utenti.nomeUtente like'%$filtroNomeUtente%'
             AND gruppi.descrizione like '%$filtroNomeGruppo%'
-            AND utenti.Gruppi_idGruppo like '%$filtroIdGruppo%'
+            AND utenti.Gruppi_idGruppo $operazione '%$filtroIdGruppo%'
             AND utenti.Gruppi_idGruppo = gruppi.idGruppo;
       ";
 
-      $this->result = $mysqli_conn->query($sql);
+      $this->result = $db->query($sql);
       $this->filtroNomeUtente = $filtroNomeUtente;
       $this->filtroNomeGruppo = $filtroNomeGruppo;
       $this->filtroIdGruppo = $filtroIdGruppo;
+      $this->lista_utenti = $this->result->fetchAll();
+    }
+
+    public function getLista() {
+      return $this->lista_utenti;
     }
 
     public function stampaLista() {
-      if ($this->result->num_rows > 0) {
-        while($row = $this->result->fetch_assoc()) {
-          echo "id: ".$row["idUtente"]." - Name: ".$row["nomeUtente"]."<br>";
-        }
-      } else {
-        echo "0 results";
-      }
+      echo '<pre>';
+      print_r($this->lista_utenti);
+      echo '</pre>';
     }
 
   }
--- 02_Codice/htdocs/FormazioneDocentiTest/testquery.php
+++ 02_Codice/htdocs/FormazioneDocentiTest/testquery.php
@@ -1,10 +1,14 @@
 <?php
   require $_SERVER['DOCUMENT_ROOT']."/FormazioneDocenti/baseUrl.php";
   require_once "$__ROOT__/tierData/DataModel/listaUtenti.php";
+  require_once "$__ROOT__/tierData/dbInterface/CommonDB.php";
 
-  $lista1 = new listaUtenti($mysqli_conn, "test1");
+  $db = connectDB();
+  $lista = array();
+
+  //$lista1 = new listaUtenti($mysqli_conn, "test1");
   //$lista1 = new listaUtenti($mysqli_conn, null, "Amministratori");
-  //$lista1 = new listaUtenti($mysqli_conn, "test0", null, 3);
+  $lista1 = new ListaUtenti($lista, $mysqli_conn, "test0", null, 3, ">");
   //$lista1 = new listaUtenti($mysqli_conn, "test", "Docenti", 1);
   $lista1->stampaLista();
 ?>
